13a3e70d0e459d388023847d4d0f748a7785cb15
mrtaz77 <mrtazwar23@gmail.com>
2024-08-20 11:40:03 +0600

    favicon working; remove next/head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +16,6 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<Head>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
